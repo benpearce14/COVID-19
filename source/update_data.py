165b3f982d5d29068a7c87088cd25c29317af8e2
Ben Pearce <b.j.pearce@durham.ac.uk>
2020-04-18 18:36:53 +0900

    Update update_data.py

--- source/update_data.py
+++ source/update_data.py
@@ -2,5 +2,5 @@ import urllib.request
 
 print('Beginning data update...')
 url = 'https://pomber.github.io/covid19/timeseries.json'
-urllib.request.urlretrieve(url, '../Data/world.json')
+urllib.request.urlretrieve(url, '../data/world.json')
 print('Data has been updated')
