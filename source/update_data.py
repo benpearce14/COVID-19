11e387aeb724fa86996b81ee9a53b57158d50629
Benjamin Pearce <jzwv37@Bens-iMac-Mini.local>
2020-04-18 12:13:52 +0100

    updated data updation scrpt

--- source/update_data.py
+++ source/update_data.py
@@ -1,12 +1,20 @@
 import wget
 import os
 
-data_path='../data/world.json'
+if not len(os.listdir('../data/') ) == 0:
+    os.system('rm ../data/*.csv')
 
-if os.path.exists(data_path):
-    os.system('rm %s' % data_path)
-
+print('------------------------')
 print('Beginning data update...')
-url = 'https://pomber.github.io/covid19/timeseries.json'
-wget.download(url, '../data/world.json')
-print('\nData has been updated')
+print('------------------------')
+
+url='https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/'
+wget.download('%s/UID_ISO_FIPS_LookUp_Table.csv' % url, '../data/populations.csv')
+wget.download('%s/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv' % url, '../data/confirmed.csv')
+wget.download('%s/csse_covid_19_time_series/time_series_covid19_recovered_global.csv' % url, '../data/recovered.csv')
+wget.download('%s/csse_covid_19_time_series/time_series_covid19_deaths_global.csv' % url, '../data/deaths.csv')
+
+print('\n----------------------')
+print('Data has been updated!')
+print('----------------------')
+
