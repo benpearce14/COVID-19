b6c5708d2cd6d40569c4e557511443ed15fc93ac
Benjamin Pearce <jzwv37@Bens-iMac-Mini.local>
2020-04-18 10:53:32 +0100

    download world data changed to wget for legacy

--- source/update_data.py
+++ source/update_data.py
@@ -1,6 +1,6 @@
-import urllib.request
+import wget
 
 print('Beginning data update...')
 url = 'https://pomber.github.io/covid19/timeseries.json'
-urllib.request.urlretrieve(url, '../data/world.json')
+wget.download(url, '../data/world.json')
 print('Data has been updated')
