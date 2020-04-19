8bf9b68ca729c3313d3789bfd2c1e394e2ffa9f6
Ben Pearce <b.j.pearce@durham.ac.uk>
2020-04-19 18:23:19 +0900

    Update extract_data.py

--- source/extract_data.py
+++ source/extract_data.py
@@ -47,6 +47,10 @@ def extract_countries(country_list):
                 confirmed = confirmed.iloc[0,4:].values
                 recovered = recovered.iloc[0,4:].values
                 deaths = deaths.iloc[0,4:].values
+        else:
+            confirmed = confirmed.iloc[0,4:].values
+            recovered = recovered.iloc[0,4:].values
+            deaths = deaths.iloc[0,4:].values
 
         countries_info.append([country_name, population, np.asarray(dates), np.asarray(confirmed), \
                                np.asarray(recovered), np.asarray(deaths)])
