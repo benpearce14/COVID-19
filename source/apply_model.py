24aa2caff2697e933de645134bcef3a50c97d0b2
Ben Pearce <b.j.pearce@durham.ac.uk>
2020-04-20 01:15:38 +0900

    Update apply_model.py

--- source/apply_model.py
+++ source/apply_model.py
@@ -22,11 +22,11 @@ daystot = 200
 Ns, Nw, delta_Ns, delta_Nw = md.CreateDataframes(pop, frac_fat, c_0, daystot)
 for d in range(0, daystot):
     if d > 3:
-				k_s = 0.682
-				k_w = k_s
-			if d > 52:
-				k_s = 0.026
-				k_w = k_s
+	k_s = 0.682
+	k_w = k_s
+    if d > 52:
+	k_s = 0.026
+	k_w = k_s
     Ns, Nw, delta_Ns, delta_Nw = md.PredictNextDay(Ns,Nw, delta_Ns, delta_Nw ,d, k_s, k_w)
 
 pr.plot_country_evolution(daystot, countries_info[0], Nw)
